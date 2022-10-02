cdf00264569f4e4ab2afe49c8fda0d3ac69b6d31
RipperOnik <kingstrong2@mail.ru>
2022-10-03 01:19:46 +0300

    added the db url

--- src/main/java/easyrecipe/web/easyrecipeWEB/dao/DaoInstance.java
+++ src/main/java/easyrecipe/web/easyrecipeWEB/dao/DaoInstance.java
@@ -11,9 +11,9 @@ public class DaoInstance {
 
     public static Dao getDAO(){
         dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/EasyRecipe");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("42248019");
+        dataSource.setUrl("postgres://jiahintwtnaiie:59b7911402c6a0f69a520467439af9a07ed23f0910451b2440de20be3ea1419a@ec2-54-155-110-181.eu-west-1.compute.amazonaws.com:5432/ddqrpa8c00m01f");
+        dataSource.setUsername("jiahintwtnaiie");
+        dataSource.setPassword("59b7911402c6a0f69a520467439af9a07ed23f0910451b2440de20be3ea1419a");
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
         Dao dao = new Dao(jdbcTemplate);
         return dao;
